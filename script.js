687ef92164c0c0f01d12c3f2ea5b0f7721514cf6
NANO-ARTRICAN <lnbmaimbo@gmail.com>
2024-08-13 11:57:44 +0200

    added hamburger button and slide animation

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingScreen = document.getElementById('loading-screen');
     
+    
     setTimeout(function () {
         loadingScreen.style.opacity = '0';
         loadingScreen.style.visibility = 'hidden';
     }, 5000); // 5 seconds delay to allow the text to animate
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    menuToggle.addEventListener('click', () => {
+        if (navMenu.classList.contains('active')) {
+            navMenu.classList.remove('active');
+            navMenu.classList.add('inactive');
+        } else {
+            navMenu.classList.remove('inactive');
+            navMenu.classList.add('active');
+        }
+        menuToggle.classList.toggle('change');
+    });
 
     const images = document.querySelectorAll("#hc2 img");
     let currentIndex = 0;
