d248e1f4aa664456326c5cc9df3223d8daffb0e3
NANO-ARTRICAN <lnbmaimbo@gmail.com>
2024-08-15 01:02:12 +0200

    vARIOUS CHANGES TO STRUCTURE IN HTML FILES

--- script.js
+++ script.js
@@ -7,18 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
         loadingScreen.style.visibility = 'hidden';
     }, 5000); // 5 seconds delay to allow the text to animate
 
-    // Image slideshow functionality
-    const images = document.querySelectorAll("#hc2 img");
-    let currentIndex = 0;
-
-    function showNextImage() {
-        images[currentIndex].classList.remove("active");
-        currentIndex = (currentIndex + 1) % images.length;
-        images[currentIndex].classList.add("active");
-    }
-
-    setInterval(showNextImage, 3000); // Change image every 3 seconds
-
     // Video hover functionality
     const portfolioItems = document.querySelectorAll('.portfolio-item');
 
